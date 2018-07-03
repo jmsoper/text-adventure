87910f49038e3b7f6b3eb9fd420a8fcf523af440
jmsoper <jmsoper@gmail.com>
2018-07-03 18:24:26 -0400

    initial commit

---
+++ basicGame.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+
+
+std::string getInput(){
+  std::string givenInput;
+  std::getline(std::cin, givenInput);
+  return givenInput;
+}
+
+std::string getHelp() {
+  std::string helper = "type in 'exit' to exit at any time. type in 'help' for help. type in 'look around' to get a description of where you are.";
+  return helper;
+}
+
+std::string getLocation(std::string location){
+  std::string prompt;
+  if (location == "home"){
+    prompt = "You're standing outside of a cabin. The rain is only getting heavier.";
+  } else {
+    prompt = "I don't know where you are.";
+  }
+  return prompt;
+}
+
+int main() {
+  std::string userInput;
+  std::string location = "home"; 
+ 
+  std::cout << "welcome to swampland. type in 'exit' to exit at any time. type in 'help' for help. type in 'look around' to get a description of where you are." << std::endl; 
+  
+  while (userInput != "exit") {
+    if (userInput == "look around"){
+      std::cout << getLocation(location) << std::endl;
+    } else if (userInput == "help"){
+      std::cout << getHelp() << std::endl;
+    } else {
+      std::cout << "It's raining." << std::endl;
+    }
+    userInput = getInput();
+  } 
+  return 0;
+}
